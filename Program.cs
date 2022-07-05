19eee0a22d271269f6de8bd3f014f927549a7d88
valeriy.osipchikov@gmail.com <prosoundmsk@mail.ru>
2022-07-05 15:06:33 +0300

    Filling block

--- Program.cs
+++ Program.cs
@@ -17,18 +17,34 @@ namespace HW6_Seminar6_Task41_programming_languages
                 return Arr;
         }
         
+        static string[] FillArray (string[] Arr, int number)
+        {
+            for (int i = 0; i < number; i++)
+                {
+                    Console.Write ($"Элемент {i}: ");
+                    Arr[i] = Console.ReadLine();
+                }
+        
+                return Arr;
+        }
 
 
         static void Main(string[] args)
         {
             Console.Clear();
                 
-                Console.Write("Введите количество элементов массива: ");
+                    Console.Write("Введите количество элементов массива: ");
                 int num = int.Parse(Console.ReadLine());
 
                 string[] table = {}, res= {};
                                
                 table = MassInit (num);
+
+                Console.WriteLine();
+                    Console.WriteLine($"Заполните массив элементами в количестве: {num}");
+                Console.WriteLine();
+
+                table = FillArray (table, num);
                 
         }        
     }
